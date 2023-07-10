e0b2e587b0858631bddcc7e5e9fae5e15437d550
Mat-Teus <86843927+Mat-Teus@users.noreply.github.com>
2023-07-10 20:37:33 -0300

    Add files via upload

---
+++ Questao4/src/questao4/Main.java
@@ -0,0 +1,20 @@
+package questao4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Moto moto = new Moto("Honda", "Vermelha", "100cc",2010,10000);
+       System.out.println(moto);
+       
+       System.out.println("\n");
+  
+       Carro carro = new Carro("Mercedes","Branca","200 cavalos",2020,60000);
+       System.out.println(carro);
+       
+       System.out.println("\n");
+       
+       Caminhao caminhao = new Caminhao("Volvo", "Preto","400 cavalos",2000,100000);
+        System.out.println(caminhao);
+    }
+    
+}
